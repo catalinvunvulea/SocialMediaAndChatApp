dba48f16b568293e9f210cc53aee4c672e02e668
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-06-01 09:27:20 +0100

    Initial Commit

---
+++ SocialMediaApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SocialMediaApp
+//
+//  Created by Dumitru Catalin Vunvulea on 01/06/2020.
+//  Copyright © 2020 Dumitru Catalin Vunvulea. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
