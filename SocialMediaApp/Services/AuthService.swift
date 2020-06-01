a5a66e026be51938748425647c57fe3a09949b53
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-06-01 15:42:59 +0100

    Setting up Auth Services - Firebase
    func registerUser and loginUser

---
+++ SocialMediaApp/Services/AuthService.swift
@@ -0,0 +1,38 @@
+//
+//  AuthService.swift
+//  SocialMediaApp
+//
+//  Created by Dumitru Catalin Vunvulea on 01/06/2020.
+//  Copyright © 2020 Dumitru Catalin Vunvulea. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthServices {
+    
+    static let instance = AuthServices()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            guard let user = authResult?.user else {
+                userCreationComplete(false, error)
+                return
+            }
+            let userData = ["provider": user.providerID, "email": user.email]  //provider id can be facebook, google, email etc
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            guard let user = authResult?.user else {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+    
+}
