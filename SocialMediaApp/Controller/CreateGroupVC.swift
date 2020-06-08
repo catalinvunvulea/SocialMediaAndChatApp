91083e630cc1f92974494b6ef365588e723a128b
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-06-08 11:33:42 +0100

    Create UserCell
    not linked yet to firebase

--- SocialMediaApp/Controller/CreateGroupVC.swift
+++ SocialMediaApp/Controller/CreateGroupVC.swift
@@ -22,14 +22,30 @@ class CreateGroupVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func doneBtnPressed(_ sender: Any) {
     }
+    
+}
+
+extension CreateGroupVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell") as? UserCell else { return UITableViewCell()}
+        let image = UIImage(named: "defaultProfileImage")
+        cell.configureCell(profileImage: image!, email: "catalin.vunvulea@gmail.com", isSelected: true)
+        return cell
+    }
+    
     
 }
---
+++ SocialMediaApp/View/UserCell.swift
@@ -0,0 +1,33 @@
+//
+//  UserCell.swift
+//  SocialMediaApp
+//
+//  Created by Dumitru Catalin Vunvulea on 08/06/2020.
+//  Copyright © 2020 Dumitru Catalin Vunvulea. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var userImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImg: UIImageView!
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+        self.userImg.image = image
+        self.emailLbl.text = email
+        if isSelected {
+            self.checkImg.isHidden = false
+        } else {
+            self.checkImg.isHidden = true
+        }
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
